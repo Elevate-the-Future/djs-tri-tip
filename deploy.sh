a01d4aef77fc820098fb73c342d5af97e651e5f8
Safin Singh <safin.singh@gmail.com>
2020-08-06 21:17:44 -0700

    Create auto-deploy workflow

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+echo "====[ SETTING UP GIT ]===="
+git config --global user.name "Safin Singh"
+git config --global user.email "safin.singh@gmail.com"
+
 echo "====[ COPYING FOLDER ]===="
 rm -rf docs/
 cp -Rf djs-tri-trip/ docs/
 
 echo "====[ UPDATING VERSION CONTROL ]===="
-read -p "Commit message: " message
-
 git add .
-git commit -m "$message"
+git commit -m "Automatically deploy"
 git push
